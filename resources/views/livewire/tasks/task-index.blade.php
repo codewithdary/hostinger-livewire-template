347e29bfaf963dc72433df271a4b70c28878efd0
Code With Dary <info@codewithdary.com>
2023-10-15 17:59:24 +0200

    Added Task Index page

---
+++ resources/views/livewire/tasks/task-index.blade.php
@@ -0,0 +1,18 @@
+<div class="rounded-md bg-pink-50 p-4 w-4/5 sm:w-2/5 mx-auto">
+    <div class="flex">
+        <div class="ml-3 mt-1 flex-1 md:flex md:justify-between">
+            <p class="text-sm font-bold text-pink-700">
+                This is Task Number 1
+            </p>
+            <p class="mt-2 text-md md:ml-6 md:mt-0">
+                <a href="/" class="whitespace-nowrap font-medium text-pink-700 hover:text-pink-600 pr-2">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </a>
+
+                <a href="/" class="whitespace-nowrap font-medium text-pink-700 hover:text-pink-600">
+                    <i class="fa-solid fa-trash"></i>
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
